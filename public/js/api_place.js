a63b513bdcdcd1128987de50198d55a2d0787cad
like12like12 <like12like12@hotmail.com>
2020-11-26 01:30:27 +0700

    1.30
    explorer link + much

--- public/js/api_place.js
+++ public/js/api_place.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $.ajaxSetup({
         headers: {
             'Authorization': 'bearer G(LsNMYQxwthtlPATlMG9zWWTmzqnzcNuK6fQeo86o0KzNSdoF2kMFReYcJ2KwTCpq8QfxXKL00BqUe7Jt4TJtW=====2',
-            'Accept-Language': 'en'
+            'Accept-Language': 'th'
         }
     });
 
@@ -10,29 +10,36 @@ $(document).ready(function() {
     const urlParams = new URLSearchParams(queryString);
     const type = urlParams.get('type');
     const id = urlParams.get('id');
-    const Hotel = "ACCOMMODATION";
-    const attraction = "ATTRACTION";
-    const restaurant = "RESTAURANT";
-    const shop = "SHOP";
-    const other = "OTHER";
+    // const Hotel = "ACCOMMODATION";
+    // const attraction = "ATTRACTION";
+    // const restaurant = "RESTAURANT";
+    // const shop = "SHOP";
+    // const other = "OTHER";
 
 
 
 
     $.getJSON("https://tatapi.tourismthailand.org/tatapi/v5/" + type + "/" + id, function(json) {
+        
         console.log(json)
         document.getElementById("place_name").innerHTML = JSON.stringify(json.result.place_name).slice(1, -1)
-        document.body.style.backgroundImage = JSON.stringify(json.result.web_picture_urls[0])
-        document.getElementById("district-province").innerHTML = "เมือง" + JSON.stringify(json.result.location.district).slice(1, -1) + " " + "จังหวัด" + JSON.stringify(json.result.location.province).slice(1, -1)
-            //document.getElementById("location").innerHTML = JSON.stringify(json.result.latitude)
+        document.getElementById("img").style = "background-image: url("+JSON.stringify(json.result.web_picture_urls[0]).slice(1, -1)+")"
+        console.log("url("+JSON.stringify(json.result.web_picture_urls[0]).slice(1, -1)+")")
+        document.getElementById("district-province").innerHTML = "เมือง " + JSON.stringify(json.result.location.district).slice(1, -1) + " จังหวัด " + JSON.stringify(json.result.location.province).slice(1, -1)
+        //document.getElementById("location").innerHTML = JSON.stringify(json.result.latitude)
         document.getElementById("overview").innerHTML = JSON.stringify(json.result.place_information.introduction).slice(1, -1)
-        document.getElementById("day1").innerHTML = JSON.stringify(json.result.opening_hours.weekday_text.day1)
-        document.getElementById("day2").innerHTML = JSON.stringify(json.result.opening_hours.weekday_text.day2)
-        document.getElementById("day3").innerHTML = JSON.stringify(json.result.opening_hours.weekday_text.day3)
-        document.getElementById("day4").innerHTML = JSON.stringify(json.result.opening_hours.weekday_text.day4)
-        document.getElementById("day5").innerHTML = JSON.stringify(json.result.opening_hours.weekday_text.day5)
-        document.getElementById("day6").innerHTML = JSON.stringify(json.result.opening_hours.weekday_text.day6)
-        document.getElementById("day7").innerHTML = JSON.stringify(json.result.opening_hours.weekday_text.day7)
+        
+        if (JSON.stringify(json.result.opening_hours.weekday_text.day1) === undefined){
+            document.getElementById("open").style.display = "none";
+        }else{
+            document.getElementById("day1").innerHTML = JSON.stringify(json.result.opening_hours.weekday_text.day1)
+            document.getElementById("day2").innerHTML = JSON.stringify(json.result.opening_hours.weekday_text.day2)
+            document.getElementById("day3").innerHTML = JSON.stringify(json.result.opening_hours.weekday_text.day3)
+            document.getElementById("day4").innerHTML = JSON.stringify(json.result.opening_hours.weekday_text.day4)
+            document.getElementById("day5").innerHTML = JSON.stringify(json.result.opening_hours.weekday_text.day5)
+            document.getElementById("day6").innerHTML = JSON.stringify(json.result.opening_hours.weekday_text.day6)
+            document.getElementById("day7").innerHTML = JSON.stringify(json.result.opening_hours.weekday_text.day7)
+        }
     });
 
 
--- public/js/api_search.js
+++ public/js/api_search.js
@@ -8,41 +8,42 @@ $(document).ready(function() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const keyword = urlParams.get('keyword');
-    const destinations = urlParams.get('destinations');
-    const catagories = urlParams.get('catagories');
+    const destination = urlParams.get('destination');
+    const catagory = urlParams.get('catagory');
     document.getElementById('keyword').value = keyword;
-    document.getElementById('destinations').value = destinations;
-    document.getElementById('catagories').value = catagories;
-    const api = "https://tatapi.tourismthailand.org/tatapi/v5/places/search?keyword=" + keyword + "&categories=" + catagories + "&provinceName=";
-    const decodeurl = decodeURIComponent(api);
+    document.getElementById('destination').value = destination;
+    document.getElementById('catagory').value = catagory;
+    const api = "https://tatapi.tourismthailand.org/tatapi/v5/places/search?keyword=" + keyword + "&categories=" + catagory + "&provinceName=";
+    // const decodeurl = decodeURIComponent(api);
 
 
-    $.getJSON(decodeurl, function(json) {
+    $.getJSON(api, function(json) {
         console.log(json)
+        console.log("single-listing?type=" + JSON.stringify(json.result[0].category_code).slice(1, -1) + "&id=" + JSON.stringify(json.result[0].place_id).slice(1, -1))
         document.getElementById("01").innerHTML = JSON.stringify(json.result[0].place_name).slice(1, -1)
-            //document.getElementById("11").href = JSON.stringify(json.result[0].place_id).slice(1, -1)
         document.getElementById("02").innerHTML = JSON.stringify(json.result[0].destination).slice(1, -1)
         document.getElementById("03").src = JSON.stringify(json.result[0].thumbnail_url).slice(1, -1)
-
+        document.getElementById("04").href = "single-listing?type=" + JSON.stringify(json.result[0].category_code).slice(1, -1) + "&id=" + JSON.stringify(json.result[0].place_id).slice(1, -1)
+        
         document.getElementById("11").innerHTML = JSON.stringify(json.result[1].place_name).slice(1, -1)
-            //document.getElementById("11").href = JSON.stringify(json.result[1].place_id).slice(1, -1)
         document.getElementById("12").innerHTML = JSON.stringify(json.result[1].destination).slice(1, -1)
         document.getElementById("13").src = JSON.stringify(json.result[1].thumbnail_url).slice(1, -1)
+        document.getElementById("14").href = "single-listing?type=" + JSON.stringify(json.result[1].category_code).slice(1, -1) + "&id=" + JSON.stringify(json.result[1].place_id).slice(1, -1)
 
         document.getElementById("21").innerHTML = JSON.stringify(json.result[2].place_name).slice(1, -1)
-            //document.getElementById("11").href = JSON.stringify(json.result[2].place_id).slice(1, -1)
         document.getElementById("22").innerHTML = JSON.stringify(json.result[2].destination).slice(1, -1)
         document.getElementById("23").src = JSON.stringify(json.result[2].thumbnail_url).slice(1, -1)
+        document.getElementById("24").href = "single-listing?type=" + JSON.stringify(json.result[2].category_code).slice(1, -1) + "&id=" + JSON.stringify(json.result[2].place_id).slice(1, -1)
 
         document.getElementById("31").innerHTML = JSON.stringify(json.result[3].place_name).slice(1, -1)
-            //document.getElementById("11").href = JSON.stringify(json.result[3].place_id).slice(1, -1)
         document.getElementById("32").innerHTML = JSON.stringify(json.result[3].destination).slice(1, -1)
         document.getElementById("33").src = JSON.stringify(json.result[3].thumbnail_url).slice(1, -1)
+        document.getElementById("34").href = "single-listing?type=" + JSON.stringify(json.result[3].category_code).slice(1, -1) + "&id=" + JSON.stringify(json.result[3].place_id).slice(1, -1)
 
         document.getElementById("41").innerHTML = JSON.stringify(json.result[4].place_name).slice(1, -1)
-            //document.getElementById("11").href = JSON.stringify(json.result[4].place_id).slice(1, -1)
         document.getElementById("42").innerHTML = JSON.stringify(json.result[4].destination).slice(1, -1)
         document.getElementById("43").src = JSON.stringify(json.result[4].thumbnail_url).slice(1, -1)
+        document.getElementById("44").href = "single-listing?type=" + JSON.stringify(json.result[4].category_code).slice(1, -1) + "&id=" + JSON.stringify(json.result[4].place_id).slice(1, -1)
 
 
     });
