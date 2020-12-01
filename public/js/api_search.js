b5ca802af084bcb1304fb8f81fdd512d9dcc0ffc
like12like12 <like12like12@hotmail.com>
2020-12-02 00:45:17 +0700

    0.45
    cookie fav

--- public/js/api_search.js
+++ public/js/api_search.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    place_id
     $.ajaxSetup({
         headers: {
             'Authorization': 'bearer G(LsNMYQxwthtlPATlMG9zWWTmzqnzcNuK6fQeo86o0KzNSdoF2kMFReYcJ2KwTCpq8QfxXKL00BqUe7Jt4TJtW=====2',
@@ -23,7 +24,6 @@ $(document).ready(function() {
     }
     const decodeurl = decodeURIComponent(api);
 
-
     $.getJSON(decodeurl, function(json) {
         console.log(json)
         address = JSON.stringify(json.result[0].latitude).slice(1, -1)+","+JSON.stringify(json.result[0].longitude).slice(1, -1)
@@ -32,27 +32,30 @@ $(document).ready(function() {
         document.getElementById("02").innerHTML = JSON.stringify(json.result[0].destination).slice(1, -1)
         document.getElementById("03").src = JSON.stringify(json.result[0].thumbnail_url).slice(1, -1)
         document.getElementById("04").href = "single-listing?type=" + JSON.stringify(json.result[0].category_code).slice(1, -1) + "&id=" + JSON.stringify(json.result[0].place_id).slice(1, -1)
+        place_id[0] = json.result[0].place_id
         
         document.getElementById("11").innerHTML = JSON.stringify(json.result[1].place_name).slice(1, -1)
         document.getElementById("12").innerHTML = JSON.stringify(json.result[1].destination).slice(1, -1)
         document.getElementById("13").src = JSON.stringify(json.result[1].thumbnail_url).slice(1, -1)
         document.getElementById("14").href = "single-listing?type=" + JSON.stringify(json.result[1].category_code).slice(1, -1) + "&id=" + JSON.stringify(json.result[1].place_id).slice(1, -1)
+        place_id[1] = json.result[1].place_id
 
         document.getElementById("21").innerHTML = JSON.stringify(json.result[2].place_name).slice(1, -1)
         document.getElementById("22").innerHTML = JSON.stringify(json.result[2].destination).slice(1, -1)
         document.getElementById("23").src = JSON.stringify(json.result[2].thumbnail_url).slice(1, -1)
         document.getElementById("24").href = "single-listing?type=" + JSON.stringify(json.result[2].category_code).slice(1, -1) + "&id=" + JSON.stringify(json.result[2].place_id).slice(1, -1)
+        place_id[2] = json.result[2].place_id
 
         document.getElementById("31").innerHTML = JSON.stringify(json.result[3].place_name).slice(1, -1)
         document.getElementById("32").innerHTML = JSON.stringify(json.result[3].destination).slice(1, -1)
         document.getElementById("33").src = JSON.stringify(json.result[3].thumbnail_url).slice(1, -1)
         document.getElementById("34").href = "single-listing?type=" + JSON.stringify(json.result[3].category_code).slice(1, -1) + "&id=" + JSON.stringify(json.result[3].place_id).slice(1, -1)
+        place_id[3] = json.result[3].place_id
 
         document.getElementById("41").innerHTML = JSON.stringify(json.result[4].place_name).slice(1, -1)
         document.getElementById("42").innerHTML = JSON.stringify(json.result[4].destination).slice(1, -1)
         document.getElementById("43").src = JSON.stringify(json.result[4].thumbnail_url).slice(1, -1)
         document.getElementById("44").href = "single-listing?type=" + JSON.stringify(json.result[4].category_code).slice(1, -1) + "&id=" + JSON.stringify(json.result[4].place_id).slice(1, -1)
-
-
+        place_id[4] = json.result[4].place_id
     });
 });
