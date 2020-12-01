532ee6b2f11100d1db58ab3db833c57ca7d7d4ca
like12like12 <like12like12@hotmail.com>
2020-12-02 01:37:44 +0700

    1.36
    fav cookie cleaned

--- public/js/api_search.js
+++ public/js/api_search.js
@@ -1,5 +1,7 @@
-$(document).ready(function() {
-    place_id
+var place_id = [""]
+function setup() {
+
+
     $.ajaxSetup({
         headers: {
             'Authorization': 'bearer G(LsNMYQxwthtlPATlMG9zWWTmzqnzcNuK6fQeo86o0KzNSdoF2kMFReYcJ2KwTCpq8QfxXKL00BqUe7Jt4TJtW=====2',
@@ -15,8 +17,7 @@ $(document).ready(function() {
     document.getElementById('destination').value = destination;
     document.getElementById('catagory').value = catagory;
     api = "https://tatapi.tourismthailand.org/tatapi/v5/places/search?keyword=" + keyword + "&categories=" + catagory + "&provinceName=" + destination;
-    if(keyword&&destination&&catagory){
-    }else{
+    if (keyword && destination && catagory) {} else {
         document.getElementById('keyword').value = keyword;
         document.getElementById('destination').value = "Bangkok";
         document.getElementById('catagory').value = "ALL";
@@ -26,36 +27,34 @@ $(document).ready(function() {
 
     $.getJSON(decodeurl, function(json) {
         console.log(json)
-        address = JSON.stringify(json.result[0].latitude).slice(1, -1)+","+JSON.stringify(json.result[0].longitude).slice(1, -1)
-        console.log(address)
         document.getElementById("01").innerHTML = JSON.stringify(json.result[0].place_name).slice(1, -1)
         document.getElementById("02").innerHTML = JSON.stringify(json.result[0].destination).slice(1, -1)
         document.getElementById("03").src = JSON.stringify(json.result[0].thumbnail_url).slice(1, -1)
         document.getElementById("04").href = "single-listing?type=" + JSON.stringify(json.result[0].category_code).slice(1, -1) + "&id=" + JSON.stringify(json.result[0].place_id).slice(1, -1)
-        place_id[0] = json.result[0].place_id
-        
+        place_id[0] = JSON.stringify(json.result[0].place_id).slice(1, -1)
+
         document.getElementById("11").innerHTML = JSON.stringify(json.result[1].place_name).slice(1, -1)
         document.getElementById("12").innerHTML = JSON.stringify(json.result[1].destination).slice(1, -1)
         document.getElementById("13").src = JSON.stringify(json.result[1].thumbnail_url).slice(1, -1)
         document.getElementById("14").href = "single-listing?type=" + JSON.stringify(json.result[1].category_code).slice(1, -1) + "&id=" + JSON.stringify(json.result[1].place_id).slice(1, -1)
-        place_id[1] = json.result[1].place_id
+        place_id[1] = JSON.stringify(json.result[1].place_id).slice(1, -1)
 
         document.getElementById("21").innerHTML = JSON.stringify(json.result[2].place_name).slice(1, -1)
         document.getElementById("22").innerHTML = JSON.stringify(json.result[2].destination).slice(1, -1)
         document.getElementById("23").src = JSON.stringify(json.result[2].thumbnail_url).slice(1, -1)
         document.getElementById("24").href = "single-listing?type=" + JSON.stringify(json.result[2].category_code).slice(1, -1) + "&id=" + JSON.stringify(json.result[2].place_id).slice(1, -1)
-        place_id[2] = json.result[2].place_id
+        place_id[2] = JSON.stringify(json.result[2].place_id).slice(1, -1)
 
         document.getElementById("31").innerHTML = JSON.stringify(json.result[3].place_name).slice(1, -1)
         document.getElementById("32").innerHTML = JSON.stringify(json.result[3].destination).slice(1, -1)
         document.getElementById("33").src = JSON.stringify(json.result[3].thumbnail_url).slice(1, -1)
         document.getElementById("34").href = "single-listing?type=" + JSON.stringify(json.result[3].category_code).slice(1, -1) + "&id=" + JSON.stringify(json.result[3].place_id).slice(1, -1)
-        place_id[3] = json.result[3].place_id
+        place_id[3] = JSON.stringify(json.result[3].place_id).slice(1, -1)
 
         document.getElementById("41").innerHTML = JSON.stringify(json.result[4].place_name).slice(1, -1)
         document.getElementById("42").innerHTML = JSON.stringify(json.result[4].destination).slice(1, -1)
         document.getElementById("43").src = JSON.stringify(json.result[4].thumbnail_url).slice(1, -1)
         document.getElementById("44").href = "single-listing?type=" + JSON.stringify(json.result[4].category_code).slice(1, -1) + "&id=" + JSON.stringify(json.result[4].place_id).slice(1, -1)
-        place_id[4] = json.result[4].place_id
+        place_id[4] = JSON.stringify(json.result[4].place_id).slice(1, -1)
     });
-});
+}
