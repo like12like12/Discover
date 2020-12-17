fde846aabc818771baa5c59b4a55b702acc33703
Akarawin <like12like12@hotmail.com>
2020-12-17 13:03:14 +0700

    real final

--- public/js/api_explorer.js
+++ public/js/api_explorer.js
@@ -3,23 +3,28 @@ var latlong = ""
 var lat = [""]
 var lng = [""]
 function setupexplorer() {
+    //get fav as array
     arr = JSON.parse(localStorage.getItem("array"))
-    console.log("fav= " + arr)
+        // console.log(arr)
+    //set api key
     $.ajaxSetup({
         headers: {
             'Authorization': 'bearer G(LsNMYQxwthtlPATlMG9zWWTmzqnzcNuK6fQeo86o0KzNSdoF2kMFReYcJ2KwTCpq8QfxXKL00BqUe7Jt4TJtW=====2',
             'Accept-Language': 'th'
         }
     });
+    //get parameter
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const keyword = urlParams.get('keyword');
     const destination = urlParams.get('destination');
     const catagory = urlParams.get('catagory');
+    //fill textbox
     document.getElementById('keyword').value = keyword;
     document.getElementById('destination').value = destination;
     document.getElementById('catagory').value = catagory;
     api = "https://tatapi.tourismthailand.org/tatapi/v5/places/search?keyword=" + keyword + "&categories=" + catagory + "&provinceName=" + destination + "&numberOfResult=5";
+    //check if parameter empty
     if (!keyword && !destination && !catagory) {
         document.getElementById('keyword').value = keyword;
         document.getElementById('destination').value = "Bangkok";
@@ -31,7 +36,7 @@ function setupexplorer() {
     //getjson
     $.getJSON(decodeurl, function(json) {
         //result
-        console.log("result" + json)
+            // console.log(json)
         //loop output
         for (let i = 0; i < json.result.length; i++) {
             $.getJSON(api, function(json) {
@@ -53,8 +58,9 @@ function setupexplorer() {
                 //latitude longitude
                 lat[i] = JSON.stringify(json.result[i].latitude) + ','
                 lng[i] = JSON.stringify(json.result[i].longitude)
-                //fav
+                //fav id
                 place_id[i] = JSON.stringify(json.result[i].category_code).slice(1, -1) + "/" + JSON.stringify(json.result[i].place_id).slice(1, -1)
+                //fav icon
                 favid = JSON.stringify(json.result[i].category_code).slice(1, -1) + "/" + JSON.stringify(json.result[i].place_id).slice(1, -1)
                 try{
                     if (arr.includes(favid)){
